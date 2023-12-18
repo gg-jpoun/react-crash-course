e1b5991c7f358d05ebc34c03e1ee789d53f3cb3c
gg-jpoun <ggpoun18@gmail.com>
2023-12-18 17:36:19 +0530

    first custom component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import Post from './components/Post'
+
 function App(){
  
-  return <h1>Hello World</h1>;
+  return <Post/>;
   
 }
 
---
+++ src/components/Post.jsx
@@ -0,0 +1,11 @@
+
+function Post() {
+    return (
+        <div>
+            <p>Guna Poun</p>
+            <p>React.js is awesome</p>
+        </div>
+    )
+}
+
+export default Post;
